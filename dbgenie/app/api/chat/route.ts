42ba0b4a86c46cb2846956fa55f800d0cd11f698
Aman <amanguptaa127@gmail.com>
2025-12-09 20:42:31 +0530

    Added system prompt ...

--- dbgenie/app/api/chat/route.ts
+++ dbgenie/app/api/chat/route.ts
@@ -1,15 +1,43 @@
 import { groq } from "@/lib/grok";
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
+import { log } from "console";
+import z from "zod";
 
 //Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
+  const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query their database using natural language.
+  You have access to following tools:
+    1. schema tool - call this  tool to get the database schema   which will help you to write sql query.
+    2. db tool - call this tool to query the database.
+  
+  
+  Rules:
+  - Generate ONLY SELECT queries( no INSERT, UPDATE, DELETE, DROP)
+  - Return valid SQLite syntax
+  
+  Always respond in a helpful, conversational tone while being technically accurate.`;
 
   const result = streamText({
     model: groq("llama-3.1-8b-instant"),
     messages: convertToModelMessages(messages),
+    system: SYSTEM_PROMPT,
+    tools: {
+      db: tool({
+        description: 'Call this tool to query a database',
+        inputSchema: z.object({
+          query: z.string().describe('SQL QUERY to be ran'),
+        }),
+        execute: async ({ query }) => {
+          console.log('Query', query);
+
+          return '';
+          
+        },
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
