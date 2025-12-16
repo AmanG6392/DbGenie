e53f9c71748ff2c8b799e8e4f55d355674200d6f
Aman <amanguptaa127@gmail.com>
2025-12-16 13:39:52 +0530

    setting up database and drizzle (ORM) connection

--- dbgenie/src/app/api/chat/route.ts
+++ dbgenie/src/app/api/chat/route.ts
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
         execute: async ({ query }) => {
           console.log('Query', query);
 
-          return '';
+          return ;
           
         },
       }),
