6459552f8a86500a88970355319466e8b2ad4954
Aman <amanguptaa127@gmail.com>
2025-12-19 13:08:10 +0530

    introducing LLM to DB

--- dbgenie/src/app/api/chat/route.ts
+++ dbgenie/src/app/api/chat/route.ts
@@ -13,9 +13,9 @@ export async function POST(req: Request) {
     1. schema tool - call this  tool to get the database schema   which will help you to write sql query.
     2. db tool - call this tool to query the database.
   
-  
   Rules:
   - Generate ONLY SELECT queries( no INSERT, UPDATE, DELETE, DROP)
+  - Always use the schema provided by the schema tool
   - Return valid SQLite syntax
   
   Always respond in a helpful, conversational tone while being technically accurate.`;
@@ -25,6 +25,32 @@ export async function POST(req: Request) {
     messages: convertToModelMessages(messages),
     system: SYSTEM_PROMPT,
     tools: {
+      schema: tool({
+        description: 'Call this tool to get database schema information.',
+        inputSchema: z.object({ }),
+        execute: async () => {
+          return `CREATE TABLE products (
+          	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+          	name text NOT NULL,
+          	category text NOT NULL,
+          	price real NOT NULL,
+          	stock integer DEFAULT 0 NOT NULL,
+          	created_at text DEFAULT CURRENT_TIMESTAMP
+         );
+         --> statement-breakpoint
+          CREATE TABLE sales (
+          	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+          	product_id integer NOT NULL,
+          	quantity integer NOT NULL,
+          	total_amount real NOT NULL,
+          	sale_date text DEFAULT CURRENT_TIMESTAMP,
+          	customer_name text NOT NULL,
+          	region text NOT NULL,
+          	FOREIGN KEY (product_id) REFERENCES products(id) ON UPDATE no action ON DELETE no action
+          );
+        `;
+        },
+      }),
       db: tool({
         description: 'Call this tool to query a database',
         inputSchema: z.object({
@@ -33,7 +59,7 @@ export async function POST(req: Request) {
         execute: async ({ query }) => {
           console.log('Query', query);
 
-          return ;
+          return query ;
           
         },
       }),
