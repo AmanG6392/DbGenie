26d404012645354b55c7476806dd5170bb2cc24e
Aman <amanguptaa127@gmail.com>
2026-01-25 14:19:15 +0530

    added some new rules to system instruction and added query validator

--- dbgenie/src/app/api/chat/route.ts
+++ dbgenie/src/app/api/chat/route.ts
@@ -1,22 +1,27 @@
 import { groq } from "@/lib/grok";
-import { streamText, UIMessage, convertToModelMessages, tool } from 'ai';
-import { log } from "console";
+import { streamText, UIMessage, convertToModelMessages, tool, stepCountIs } from 'ai';
 import z from "zod";
+import { db } from "@/src/db/db";
 
 //Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
+
   const { messages }: { messages: UIMessage[] } = await req.json();
+  
   const SYSTEM_PROMPT = `You are an expert SQL assistant that helps users to query their database using natural language.
   You have access to following tools:
-    1. schema tool - call this  tool to get the database schema   which will help you to write sql query.
-    2. db tool - call this tool to query the database.
+    1. db tool - call this tool to query the database.
+    2. schema tool - call this  tool to get the database schema which will help you to write sql query.
   
   Rules:
   - Generate ONLY SELECT queries( no INSERT, UPDATE, DELETE, DROP)
   - Always use the schema provided by the schema tool
   - Return valid SQLite syntax
+  - If a column does not exist in the table being queried, you MUST JOIN the correct table.
+  - Never reference columns that are not present in the selected table.
+
   
   Always respond in a helpful, conversational tone while being technically accurate.`;
 
@@ -24,6 +29,7 @@ export async function POST(req: Request) {
     model: groq("llama-3.1-8b-instant"),
     messages: convertToModelMessages(messages),
     system: SYSTEM_PROMPT,
+    stopWhen: stepCountIs(8),
     tools: {
       schema: tool({
         description: 'Call this tool to get database schema information.',
@@ -37,7 +43,7 @@ export async function POST(req: Request) {
           	stock integer DEFAULT 0 NOT NULL,
           	created_at text DEFAULT CURRENT_TIMESTAMP
          );
-         --> statement-breakpoint
+         
           CREATE TABLE sales (
           	id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
           	product_id integer NOT NULL,
@@ -54,13 +60,24 @@ export async function POST(req: Request) {
       db: tool({
         description: 'Call this tool to query a database',
         inputSchema: z.object({
-          query: z.string().describe('SQL QUERY to be ran'),
+          query: z.string().describe('The SQL query to be ran'),
         }),
         execute: async ({ query }) => {
           console.log('Query', query);
+           // Important: make sure you sanitize/ validate (somehow) check the query
+           // string search [delete, update] --> Guardrails
+           const cleanedQuery = query.trim().toUpperCase();
 
-          return query ;
-          
+           if (!cleanedQuery.startsWith("SELECT")) {
+             throw new Error("Only SELECT queries are allowed");
+           }
+           
+           if (/DELETE|UPDATE|INSERT|DROP|ALTER|TRUNCATE/.test(cleanedQuery)) {
+             throw new Error("Dangerous SQL detected");
+           }
+           
+           return await db.run(query);
+  
         },
       }),
     },
